5c8c53d2e3a49bf7d74151cfcbc78f4d9eb01bdf
Aayush <aayush.verma@dojima.network>
2024-11-27 12:08:44 +0530

    responsive made

--- src/components/sections/Features.tsx
+++ src/components/sections/Features.tsx
@@ -53,16 +53,16 @@ export default function Features() {
 
   return (
     <div  className="relative w-full md:min-h-[70vh]" id="beta-program">
-      <div className="relative flex flex-col items-center justify-center py-14 lg:pt-32 px-8">
+      <div className="relative flex flex-col items-center justify-center py-14 lg:pt-32 px-6">
         <SectionHeading type="primary">Edulinkx Features</SectionHeading>
 
-          <div className='grid md:grid-cols-2 gap-y-2 content-center lg:grid-cols-3 py-8 gap-x-2 z-10 max-w-[85%]'>
+          <div className='grid md:grid-cols-2 gap-y-2 content-center lg:grid-cols-3 py-8 gap-x-2'>
           {cards.map((card, index) => (
             <div key={index} className='transition-all py-10 flex flex-col justify-start px-5 bg-neutral-900 rounded-3xl opacity-90'>
               <div className='flex justify-between'>
-                <div className='h-6 w-6 text-brand-400'>{card.icon}</div>
+                <div className='h-6 w-9 text-brand-400 mb-4 '>{card.icon}</div>
               </div>
-              <h2 className='mt-2 md:mt-7 text-neutral-200 text-2xl font-semibold leading-6 max-w-[80%]'>{card.title}</h2>
+              <h2 className='mt-2 md:mt-7 text-neutral-200 text-2xl font-semibold leading-6 max-w-[80%] pb-2'>{card.title}</h2>
               <h3 className='mt-2 md:mt-7 text-zinc-400 text-xl leading-6 max-w-[80%]'>{card.desc}</h3>
             </div>
           ))}
